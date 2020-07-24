a4cfe21a635b71d7ef92982f8c41e27955a3ce7a
SuryankDixit <suryankdixit@gmail.com>
2020-07-24 22:13:32 +0530

    Added Revies table

--- server/databse.sql
+++ server/databse.sql
@@ -6,3 +6,12 @@ CREATE TABLE restaurents(
     location VARCHAR(50) NOT NULL,
     price_range INT NOT NULL check(price_range>=1000 AND price_range<=10000)
 );
+
+
+CREATE TABLE reviews(
+    id BIGSERIAL PRIMARY KEY NOT NULL,
+    restaurent_id BIGINT NOT NULL REFERENCES restaurents(id),
+    name VARCHAR(50),
+    review TEXT NOT NULL,
+    rating INT NOT NULL check(rating>=1 AND rating<=5)
+);
