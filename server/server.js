9c124145e768c76bf2bfa5e34ce544940f407bea
SuryankDixit <suryankdixit@gmail.com>
2020-07-24 22:13:48 +0530

    Updated.

--- server/server.js
+++ server/server.js
@@ -1,12 +1,21 @@
 require("dotenv").config();
 
 const express = require("express"),
+    path = require("path"),
     bodyparser = require("body-parser"),
+    methodOverride = require("method-override"),
     app = express();
 const pool = require("./config/db_config");
 const IndexRouter = require("./controllers/index.router");
 
+
+app.use(express.static('public'));
+app.set('views', path.join(__dirname, './client/views'));
+app.set('view engine', 'ejs');
+
+app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
+app.use(methodOverride("_method"));
 
 //ROUTES
 
