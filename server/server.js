ebd2bfde44f562c3c60ff024cd101f46408c372a
SuryankDixit <suryankdixit@gmail.com>
2020-07-24 12:04:56 +0530

    Refactored file

--- server/server.js
+++ server/server.js
@@ -1,7 +1,16 @@
 require("dotenv").config();
+
 const express = require("express"),
     bodyparser = require("body-parser"),
     app = express();
+const pool = require("./config/db_config");
+const IndexRouter = require("./controllers/index.router");
+
+app.use(bodyparser.json());
+
+//ROUTES
+
+app.use("/api", IndexRouter);
 
 const port = process.env.PORT || 8080;
 app.listen(port, function(req, res) {
